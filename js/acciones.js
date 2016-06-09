6d2bb8ffec883639a404648d469b267ad4b6394f
rodrigoDoradoGtz <rodrigodgtz3f.desaupoo@gmail.com>
2016-06-09 13:54:54 -0500

    9nasubida

--- js/acciones.js
+++ js/acciones.js
@@ -57,7 +57,6 @@ $(document).ready(function(e) {
 							var fila = resultado.rows.item(i);
 							var v_nombre = fila.nombre;
 							var v_apellido = fila.apellido;
-							alert(v_nombre);
 							a_html += "<li>" + v_nombre + "&nbsp;" + v_apellido + "</li>";
 						}
 					}
